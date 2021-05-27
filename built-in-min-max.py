3fb42d87be326492f9a4486f685bb5eddedba665
grimsley217 <84026008+grimsley217@users.noreply.github.com>
2021-05-26 20:00:35 -0500

    Update built-in-min-max.py

--- built-in-min-max.py
+++ built-in-min-max.py
@@ -3,23 +3,17 @@
 
 #function that calls the MAXIMUM value of 3 given values
 def maximum(x, y, z):
-  max_val=x #stores 1st value as the highest value, for now
+  max_val=x #assumes 1st value as the highest value, for now
   if y>x:
     max_val=y
   if z>y:
     max_val=z
   print('The maximum value is: ', max_val)
 
-
 #function that calls the MAXIMUM value of any number of values
-def minimum():
-  list1=[] # creating an empty list
-  x=int(input("MINIMUM: How many elements do you want in your list? "))
-  for i in range(0, x):
-    y = int(input("Enter next value: "))
-    list1.append(y) #adds each inputted value to the list
-  
-  min_val=list1[0] # stores the 1st value (index 0) as the smallest value, for now
+def minimum(x, y, *z):
+  list1=[x, y, *z] #stores all inputted values into a list   
+  min_val=list1[0] #assumes the 1st value (index 0) as the smallest value, for now
   for i in list1:
    if i<min_val:
      min_val=i
