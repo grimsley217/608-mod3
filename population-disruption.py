2cbfbb673d6e918b87ae97348afa4e433c8fa450
grimsley217 <84026008+grimsley217@users.noreply.github.com>
2021-05-26 21:18:41 -0500

    Update population-disruption.py

--- population-disruption.py
+++ population-disruption.py
@@ -1,1 +1,14 @@
+#population-dispersion.py
+"""Uses the statistics module to calculate measures of dispersion."""
 
+import statistics as stats
+
+#creates list of dice roll values to be easily used later
+rolls=[1, 3, 4, 2, 6, 5, 3, 4, 5, 2]
+
+x=stats.pvariance(rolls)
+y=stats.pstdev(rolls)
+
+print(rolls)
+print('The population variance is: ', x)
+print('The standard deviation is: ', y)
