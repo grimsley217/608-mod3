9328a40322dad136c16560413c8bea7007e31dc1
grimsley217 <84026008+grimsley217@users.noreply.github.com>
2021-05-26 20:39:49 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,13 @@
+#calculator.py
+"""This calls various MATH functions."""
 
+import math
+
+x=int(input('Enter a value to take the SQUARE ROOT of: '))
+y=float(input('Enter a value to calculate the FLOOR of: '))
+
+a=math.sqrt(x)
+b=math.floor(y)
+
+print(f'The square root of {x} is {a}.')
+print(f'The floor of {y} is {b}.')
